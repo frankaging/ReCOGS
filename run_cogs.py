a70dc9a13a84dfb9bd1e26cb8336faee953d7a83
frankaging <zxw0424@gmail.com>
2023-03-13 03:40:06 +0000

    update

--- run_cogs.py
+++ run_cogs.py
@@ -452,8 +452,8 @@ for lf in args.lfs.split(";"):
                 for i in range(len(decoded_preds)):
 
                     # use conj-based eval function.
-                    if translate_invariant_form(decoded_labels) == \
-                        translate_invariant_form(decoded_preds):
+                    if translate_invariant_form(decoded_labels[i]) == \
+                        translate_invariant_form(decoded_preds[i]):
                         correct_count += 1
                     else:
                         pass
@@ -497,8 +497,8 @@ for lf in args.lfs.split(";"):
 
                     # use conj-based eval function.
                     cat = gen_dataset.eval_cat[total_count]
-                    if translate_invariant_form(decoded_labels) == \
-                        translate_invariant_form(decoded_preds):
+                    if translate_invariant_form(decoded_labels[i]) == \
+                        translate_invariant_form(decoded_preds[i]):
                         correct_count += 1
                         per_cat_eval[cat][0] += 1
                         if cat == "obj_pp_to_subj_pp":
